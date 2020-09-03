85432647586debce37d0ceb3ec14bc0da149793c
abdulkani.ozbey <abdulkani.ozbey@maysiber.com>
2020-09-03 11:03:30 +0300

    Swagger is added to the API

--- Products.API/Startup.cs
+++ Products.API/Startup.cs
@@ -1,9 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Products.API.Data;
 using Products.API.Service;
 
@@ -21,6 +23,21 @@ namespace Products.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "A Simple API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Abdulkani Özbey",
+                        Email = "adulkaniozbey@gmail.com",
+                        Url = new Uri("https://twitter.com/kanioz")
+                    }
+                });
+            });
             services.AddDbContext<ProductDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddTransient<IProductService, ProductService>();
@@ -42,6 +59,16 @@ namespace Products.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
